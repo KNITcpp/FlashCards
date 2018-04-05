22b78fc771557661bc7d0aefa957e47f908a4ccc
Sebastian Kucharzyk <kucharzyk.sebastian@gmail.com>
2018-04-05 23:42:27 +0200

    Added Round.h that plays whole round(Work in progress)

---
+++ FlashCards/include/Round.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#include "Flashcard.h"
+#include "StudyMode.h"
+#include <vector>
+
+
+class Flashcard;
+
+class Round
+{
+public:
+	Round(std::vector<Flashcard>& flashcards, StudyMode* studyMode);
+	size_t cardsToGo() const;
+	bool isEmpty() const;
+	void addFlashcards(std::vector<Flashcard>& flashcards);
+	void play();
+private:
+	std::vector<Flashcard>& flashcards_;
+	StudyMode* studyMode_;
+	std::chrono::system_clock::time_point currentTime_;
+private:
+	void checkResources() const;
+	void playCard(std::vector<Flashcard>::value_type& flashcard);
+	void updateCurrentTime();
+};
---
+++ FlashCards/src/Round.cpp
@@ -0,0 +1,56 @@
+﻿#include "Round.h"
+
+
+Round::Round(std::vector<Flashcard>& flashcards, StudyMode* studyMode) :
+	flashcards_(flashcards),
+	studyMode_(studyMode)
+{
+	checkResources();
+}
+
+void Round::checkResources() const
+{
+	if (studyMode_ == nullptr)
+		throw std::logic_error("Round::play() : studyMode is nullptr");
+}
+
+
+size_t Round::cardsToGo() const
+{
+	return flashcards_.size();
+}
+
+
+bool Round::isEmpty() const
+{
+	return flashcards_.empty();
+}
+
+
+void Round::addFlashcards(std::vector<Flashcard>& flashcards)
+{
+	flashcards_.insert(flashcards_.end(), flashcards.begin(), flashcards.end());
+}
+
+
+void Round::play()
+{
+	for (auto& i : flashcards_)
+	{
+		updateCurrentTime();
+		playCard(i);
+	}
+}
+
+
+void Round::updateCurrentTime()
+{
+	currentTime_ = std::chrono::system_clock::now();
+}
+
+
+void Round::playCard(std::vector<Flashcard>::value_type& flashcard)
+{
+	flashcard.addUsage(currentTime_);
+	studyMode_->performFlashcard(flashcard);
+}
