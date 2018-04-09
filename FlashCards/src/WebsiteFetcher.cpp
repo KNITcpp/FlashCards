7a7bfe7e16764a6096da7f8d1a50756b8fff7f80
Jakub Gros <kubagros@gmail.com>
2018-04-09 13:43:19 +0200

    fixed problem data fetching and refactored, DikiApi working version

--- FlashCards/src/WebsiteFetcher.cpp
+++ FlashCards/src/WebsiteFetcher.cpp
@@ -1,52 +1,18 @@
 ﻿#include "WebsiteFetcher.h"
 #include <codecvt>
-#include "curl/curl.h"
 #include <exception>
 #include <iostream>
 #include <string>
 #include <locale>
+#include "curl/curl.h"
 
 
-std::wstring WebsiteFetcher::downloadWebsite(const std::wstring& link)
-{
-	//TODO: implement
-	return L"additional\\dog.html";
-}
 
-void WebsiteFetcher::loadFileIntoMemory()
-{
-	int i=0;
-	std::wstring line;
-	while(getline(fileHandler, line))
-		codeLines.push_back(line);
-}
 
-void WebsiteFetcher::closeAndDeleteFile()
-{
-	if(fileHandler.is_open())
-		fileHandler.close();
-	deleteFile();
-}
-
-void WebsiteFetcher::downloadAndOpenFile(std::wstring link)
-{
-	fetchedFilePath=downloadWebsite(link);
-	fileHandler.open(fetchedFilePath, std::wfstream::in);
-
-	if(fileHandler.good()==false)
-	throw std::exception("Invalid link. ");
-
-	const std::locale utf8_locale = std::locale(std::locale(), new std::codecvt_utf8<wchar_t>());
-	fileHandler.imbue(utf8_locale);
-}
 
 WebsiteFetcher::WebsiteFetcher(std::wstring link): link(link)
 {
 	curl_global_init(CURL_GLOBAL_ALL); //TODO: check if it's ok to call it every time new object is created (and curl_global_cleanup() in destructor also)
-
-	downloadAndOpenFile(link);
-	loadFileIntoMemory();
-	closeAndDeleteFile();
 
 	getWebsite();
 }
@@ -55,17 +21,6 @@ WebsiteFetcher::~WebsiteFetcher()
 {
 	curl_global_cleanup();
 }
-
-void WebsiteFetcher::deleteFile()
-{
-	//TODO: implement
-}
-
-struct MemoryStruct 
-{
-  char *memory;
-  size_t size;
-};
 
 static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -85,32 +40,8 @@ static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, voi
   return realsize;
 }
 
-void WebsiteFetcher::getWebsite()
+void WebsiteFetcher::convertToWstringAndSaveInVect(MemoryStruct chunk)
 {
-	struct MemoryStruct chunk;
-	chunk.memory = (char*)malloc(1);
-	chunk.size = 0;   
-	CURL* handle;
-	handle=curl_easy_init(); 
-
-
-	//curl_easy_setopt(handle, CURLOPT_FOLLOWLOCATION, 1L); //TODO: probably to delete
-	//curl_easy_setopt(handle, CURLOPT_HTTPHEADER, "Content-Type: text/plain; charset=UTF-16"); //TODO: also to delete probably
-	curl_easy_setopt(handle, CURLOPT_URL, "https://www.diki.pl//slownik-angielskiego?q=dog"); //TODO: refactor link
-	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&chunk);
-	curl_easy_setopt(handle, CURLOPT_USERAGENT, "libcurl-agent/1.0"); // some servers don't like requests that are made without a user-agent field, so we provide one
-	CURLcode result = curl_easy_perform(handle);
- 
-	if(result != CURLE_OK)
-	{
-		std::string* errMsg=new std::string("curl_easy_perform() failed: ");
-		(*errMsg)+=curl_easy_strerror(result);
-		throw std::exception(errMsg->c_str()); //TODO: memory leak, change exception
-	}
-
-	std::wfstream file;
-	file.open("C:\\asd.txt");
 	std::string line;
 	for (int i = 0; i < chunk.size / sizeof(wchar_t); ++i)
 	{
@@ -120,32 +51,54 @@ void WebsiteFetcher::getWebsite()
 			std::wstring wstr = converter.from_bytes(line);
 
 			fetchedCodeLines.push_back(wstr);
-			file << wstr << std::endl;
 			line.clear();
 		}
 		else
 			line.push_back(chunk.memory[i]);
 	}
-
- 
-  /* cleanup curl stuff */ 
-  curl_easy_cleanup(handle);
- 
-  free(chunk.memory);
 }
 
+void WebsiteFetcher::setUpRequest(MemoryStruct& chunk, CURL* & handle)
+{
+	chunk.memory = (char*)malloc(1);
+	chunk.size = 0; 
+	handle=curl_easy_init(); 
+	curl_easy_setopt(handle, CURLOPT_URL, "https://www.diki.pl//slownik-angielskiego?q=dog"); //TODO: refactor link
+	curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, (void *)&chunk);
+	curl_easy_setopt(handle, CURLOPT_USERAGENT, "libcurl-agent/1.0"); // some servers don't like requests that are made without a user-agent field, so we provide one
+}
 
+void WebsiteFetcher::getWebsite()
+{
+	struct MemoryStruct chunk;
+	CURL* handle;
+
+	setUpRequest(chunk, handle);
+	CURLcode result = curl_easy_perform(handle);
+
+	if(result != CURLE_OK)
+	{
+		std::string errMsg= std::string("curl_easy_perform() failed: ") + std::string(curl_easy_strerror(result));
+		throw CurlException(errMsg);
+	}
+
+	convertToWstringAndSaveInVect(chunk);
+	free(chunk.memory);
+ 
+	curl_easy_cleanup(handle);
+}
 
 std::wstring WebsiteFetcher::getLineOfCode(int index)
 {
 	if(getLinesQuantity()<=index || index<0)
 		throw std::out_of_range("Called function WebsiteFetcher::getLineOfCode() with invalid index.");
 
-	return codeLines[index];
+	return fetchedCodeLines[index];
 }
 
 int WebsiteFetcher::getLinesQuantity() const
 {
-	return codeLines.size();
+	return fetchedCodeLines.size();
 }
 
